6d6fef16ae24d0b355244f7418e610ace8645812
capitaine-tornado <58637162+capitaine-tornado@users.noreply.github.com>
2025-05-02 15:34:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 from flask import Flask, render_template, request
+
 app = Flask(__name__)
+
+
 @app.route('/')
 def index():
- return render_template('index.html')
-@app.route('/resultat',methods = ['GET'])
+  return render_template('index.html')
+
+
+@app.route('/resultat', methods=['GET'])
 def resultat():
-  result=request.args
+  result = request.args
   n = result['r']
   q = result['g']
   r = result['b']
   p = result['prenom']
-  return render_template("resultat.html", r=n, prenom=p, g=q,b=r)
-app.run(debug=True)
+  return render_template("resultat.html", r=n, prenom=p, g=q, b=r)
+
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=81)
